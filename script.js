8f0a3c7a84874ee268ef05e9cecd0fa64a0d5ab1
arieflazurdi <arief.gox@gmail.com>
2023-07-06 12:48:24 +0700

    update commit

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-const hamburger = document.getElementById("hamburger");
+// const hamburger = document.getElementById("hamburger");
 
-hamburger.addEventListener("click", () => {
-  const div = document.querySelector("div#mobile-menu-2");
+// hamburger.addEventListener("click", () => {
+//   const div = document.querySelector("div#mobile-menu-2");
 
-  div.classList.toggle("hidden");
-});
+//   div.classList.toggle("hidden");
+// });
 
-const listMenu = document.querySelectorAll("ul#menu li");
+// const listMenu = document.querySelectorAll("ul#menu li");
 
-listMenu.forEach((el) => {
-  el.addEventListener("click", () => {
-    el.classList.add("bg-white", "rounded-lg");
-  });
-});
+// listMenu.forEach((el) => {
+//   el.addEventListener("click", () => {
+//     el.classList.add("bg-white", "rounded-lg");
+//   });
+// });
 
-const btn = document.getElementById("hamburger");
-btn.addEventListener("click", () => {});
+// const btn = document.getElementById("hamburger");
+// btn.addEventListener("click", () => {});
 
 new Swiper(".testimonials-slider", {
   speed: 600,
